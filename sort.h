0814c8cd3085adfa2d57f2192802de5ffd8ea9f4
kenfelix <65518572+kenfelix@users.noreply.github.com>
2021-10-26 01:08:36 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -23,6 +23,5 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap(int *array, size_t x, size_t y);
 void insertion_sort_list(listint_t **list);
-void sorted_insert(listint_t **head, listint_t *newNode);
 
 #endif
